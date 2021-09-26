21c2c610a337005857413b11d96253cfa15063d1
kanishk.coe <kanishk.coe@gmail.com>
2021-09-26 12:23:44 +0530

    done more questions

--- Karan/src/Loops.java
+++ Karan/src/Loops.java
@@ -1,2 +1,41 @@
 public class Loops {
+
+    public static void main(String[] args) {
+
+        int number = 26;
+
+//        for(int currentPrintableNumber = 1; currentPrintableNumber <= number; currentPrintableNumber++) {
+//            System.out.println(currentPrintableNumber);
+//        }
+
+//        for (int i = 1; i <= 10 ; i++) {
+//            System.out.println(number * i);
+//        }
+
+        //print all the even numbers from 1 to number
+//        for (int x = 1; x <= number; x = x+2) {
+//            System.out.println(x);
+//        }
+
+//        int i = 2; // initiate
+
+//        while(i <= number) {
+//            System.out.println(i);
+//            i = i + 2;
+//        }
+
+
+        //get a sequence count (count)
+        // get a particular string (name)
+        // print that string for count times
+
+        int count = 5;
+        String name = "Karan";
+        int i = 1;
+        while(i <= count) {
+            System.out.println(name);
+            i++;
+        }
+
+    }
 }
